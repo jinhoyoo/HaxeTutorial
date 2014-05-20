67cf2e6b6c5ed90860c5371b90aa8b0643e66678
Jinho Yoo <jinhoyoo@gamebase.co.kr>
2014-05-20 19:05:39 +0900

    Added mouse event.

--- Example4/Source/Main.hx
+++ Example4/Source/Main.hx
@@ -1,9 +1,10 @@
 import flash.display.Bitmap;
 import flash.display.Sprite;
+import flash.events.MouseEvent;
 
 import openfl.Assets;
-
 import motion.Actuate;
+
 
 class Main extends Sprite {
     public function new () {
@@ -16,10 +17,16 @@ class Main extends Sprite {
         bitmap.x = 100;
         bitmap.y = 200;
 
+		bitmap.addEventListener(MouseEvent.CLICK, onClick);
 		Actuate.tween (bitmap, 3, { alpha:0.2 }).onComplete (complete);
     }
-    
+
     private function complete ():Void {
     	trace ("Hello World!");
  	}
+
+	public function onClick(e:MouseEvent):Void {
+    	trace("I've been hit!");
+	}
+ 	
 }
